0af69179ab08198ce801ff66e5a437541cf51b18
Francis Badasu <nytro04@gmail.com>
2021-03-23 05:58:57 +0000

    feat: add initial text

--- components/ContactPage.vue
+++ components/ContactPage.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="container mx-auto text-black bg-white contact">
-    <div class="sidenav">
-      <div>
-        <i
-          class="cursor-pointer hover:bg-black hover:text-white closebtn"
-          @click="close()"
-        >
-          &times;
-        </i>
-      </div>
+    <div class="px-20 pt-20 pb-10 sidenav">
+      <i
+        class="cursor-pointer hover:bg-black hover:text-white closebtn"
+        @click="close()"
+      >
+        &times;
+      </i>
+
       <div class="flex">
-        <h1 class="heading heading__primary">
-          GrainMate <br />
-          mobile app
-        </h1>
-        <p>
-          Thanks in advance for reaching out! Please note that the contact form
-          is for work only. If you just want to say hello, then hit me up on any
-          of my social media handles.
-        </p>
+        <div class="w-6/12">
+          <h1 class="text-black heading heading__primary">
+            Get in <br />
+            touch.
+          </h1>
+        </div>
+        <div class="w-6/12">
+          <p>
+            Thanks in advance for reaching out! Please note that the contact
+            form is for work only. If you just want to say hello, then hit me up
+            on any of my social media handles.
+          </p>
+        </div>
       </div>
     </div>
   </div>
