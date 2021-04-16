264a940ad6c4cd149e8975c60a5019cc182ec67f
Francis Badasu <nytro04@gmail.com>
2021-04-16 11:36:01 +0000

    feat: add responsiveness for mobile, tablet port and landscape

--- components/Video.vue
+++ components/Video.vue
@@ -2,39 +2,80 @@
   <div class="full-width">
     <div class="flex items-center justify-center video">
       <div class="relative">
-        <ArrowDownSvg class="" />
-        <PlaySvg class="play" />
+        <ShowReelSvg class="z-0 show-reel" />
+        <PlaySvg class="z-10 play" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ArrowDownSvg from '~/assets/svgs/play-showreel-badge.svg?inline'
+import ShowReelSvg from '~/assets/svgs/play-showreel-badge.svg?inline'
 import PlaySvg from '~/assets/svgs/play-btn.svg?inline'
 
 export default {
   components: {
-    ArrowDownSvg,
+    ShowReelSvg,
     PlaySvg
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import './assets/scss/abstracts/_mixins.scss';
+
 .video {
   background-color: #383838;
   height: 100vh;
+  overflow: hidden;
+
+  @include respond(tab-land) {
+    height: 25rem;
+  }
+  @include respond(tab-port) {
+    height: 25rem;
+  }
+
+  @include respond(phone) {
+    height: 14rem;
+  }
+}
+
+.show-reel {
+  @include respond(tab-land) {
+    height: 4.875rem;
+  }
+  @include respond(tab-port) {
+    height: 4.875rem;
+  }
+  @include respond(phone) {
+    height: 4.875rem;
+  }
 }
 
 .play {
   position: absolute;
   top: 5rem;
   left: 5.5rem;
+  @include respond(tab-land) {
+    height: 1rem;
+    position: absolute;
+    top: 2rem;
+  }
+  @include respond(tab-port) {
+    height: 1rem;
+    position: absolute;
+    top: 2rem;
+  }
+  @include respond(phone) {
+    height: 1rem;
+    position: absolute;
+    top: 2rem;
+  }
 }
 
-.full-width {
+/* .full-width {
   margin-left: -5rem;
   margin-right: -5rem;
-}
+} */
 </style>
